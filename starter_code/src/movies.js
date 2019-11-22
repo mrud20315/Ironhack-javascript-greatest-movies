1548f0735b4bd35aa7479aa9d162c9e1e7cc3961
Rudnick Marc <20315@columbushs.com>
2019-11-22 10:24:42 -0500

    done with 3

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,11 +1,56 @@
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear (array){
+    let copy = [...array];
+    copy.sort ((a,b)=>{
+        if(a.year < b.year){
+            return -1;
+        }
+        else if (a.title < b.title){
+            return -1;
+        }
+        else if (b.title < a.title){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    })
+    return copy;
+
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
-
+function howManyMovies(number){
+    let steven = number.filter((eachMovie)=>{
+        return eachMovie.genre.includes("Drama") && eachMovie.director == "Steven Spielberg"
+    })
+    return steven.length;
+}
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(array){
 
+    let copy = [...array];
+
+    copy.sort((a,b)=>{
+        if (a.title < b.title){
+            return -1;
+        }
+        else if (b.title < a.title){
+            return -1;
+        }
+    })
+
+
+    let top20 = copy.splice(0, 20);
+    let lastTop20 = top20.map((eachMovie)=>{
+        return eachMovie.title;
+    })
+
+    return lastTop20;
+    
+}
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
